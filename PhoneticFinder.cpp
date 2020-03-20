2b399a09ba61fb6cf64c054705e2b48c51bd7d5a
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2020-03-20 04:29:47 +0200

    Done with find() - gotta write and make tests

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,283 @@
+#include "PhoneticFinder.hpp"
+#include <iostream>
+using namespace std;
+
+namespace phonetic {
+	string find(string text, string word) {
+		string ans;
+		int index=0;
+		for (int i=0; i<text.length(); i++){
+			//if (i<word.length()) cout << word[i];
+			if (tolower(word[index]) == tolower(text[i])){
+				ans+=text[i];
+				index++;
+				if (index == word.length()){
+					if (i+1 == text.length()){ return ans; }
+					else if (text[i+1] == ' ') { return ans; }
+					else {
+							ans.clear();
+							index=0;
+						}
+				}
+			}
+			else if(tolower(word[index]) == 'v'){
+				if (tolower(text[i]) == 'w'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'w'){
+				if (tolower(text[i]) == 'v'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'b'){
+				if ((tolower(text[i]) == 'f') || (tolower(text[i]) == 'p')){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'f'){
+				if ((tolower(text[i]) == 'b') || (tolower(text[i]) == 'p')){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'p'){
+				if ((tolower(text[i]) == 'f') || (tolower(text[i]) == 'b')){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'g'){
+				if (tolower(text[i]) == 'j'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'j'){
+				if (tolower(text[i]) == 'g'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'c'){
+				if ((tolower(text[i]) == 'k') || (tolower(text[i]) == 'q')){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'k'){
+				if ((tolower(text[i]) == 'q') || (tolower(text[i]) == 'c')){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'q'){
+				if ((tolower(text[i]) == 'c') || (tolower(text[i]) == 'k')){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 's'){
+				if (tolower(text[i]) == 'z'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'z'){
+				if (tolower(text[i]) == 's'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'd'){
+				if (tolower(text[i]) == 't'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 't'){
+				if (tolower(text[i]) == 'd'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'o'){
+				if (tolower(text[i]) == 'u'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'u'){
+				if (tolower(text[i]) == 'o'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'i'){
+				if (tolower(text[i]) == 'y'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' ') { return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else if(tolower(word[index]) == 'y'){
+				if (tolower(text[i]) == 'i'){
+					ans+=text[i];
+					index++;
+					if (index == word.length()){
+						if (i+1 == text.length()){ return ans; }
+						else if (text[i+1] == ' '){ return ans; }
+						else {
+							ans.clear();
+							index=0;
+						}
+					}
+				}
+			}
+			else{
+				ans.clear();
+				index=0;
+			}
+		}
+		throw std::invalid_argument( "Can't find the word index the text !" );
+		return "--Bug--";
+	}
+}
---
+++ Test.cpp
@@ -0,0 +1,115 @@
+#include "doctest.h"
+#include "PhoneticFinder.hpp"
+using namespace phonetic;
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+TEST_CASE("Test replacement of v and w") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of b and f") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of b and p") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of f and p") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of g and j") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of c and k") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of c and q") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of k and q") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of s and z") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of d and t") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of o and u") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+TEST_CASE("Test replacement of i and y") {
+    string text = "xxx happy yyy";
+    CHECK(find(text, "happy") == string("happy"));
+    CHECK(find(text, "habby") == string("happy"));
+    CHECK(find(text, "hapby") == string("happy"));
+    CHECK(find(text, "habpy") == string("happy"));
+}
+
+
+
+
+
+TEST_CASE("Grade: 99") {
+    string text = "Happi xxx yyy";
+    CHECK(find(text, "happi") == string("Happi"));
+    CHECK(find(text, "Happi") == string("Happi"));
+    CHECK(find(text, "HAPPI") == string("Happi"));
+    CHECK(find(text, "HaPpI") == string("Happi"));
+}
